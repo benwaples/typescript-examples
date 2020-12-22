650897f7f975439dd98ae780ff12cfe63cdbc564
bwapes <benwaples@gmail.com>
2020-12-22 11:06:50 -0800

    tuple example

--- section6/tuples.ts
+++ section6/tuples.ts
@@ -1,2 +1,31 @@
 // tuple is an array like structure, each element represent some property of a record
 
+// example of a tuple: 
+const tuple = [
+  ['column1', 2, false],
+  ['row 1', 1, true],
+  ['row 2', 2, false]
+]
+
+const pepsiObject = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40
+}
+// order is very important in this case
+const pepsi = [
+  'brown', true, 40
+]
+
+// TS and tuple
+const pepsiTs: [string, boolean, number] = [
+  'brown', true, 40
+]
+
+// creating a type for your tuple
+// note how I was only using types for objects and in this case we are using is for an array.
+type DrinkType = [string, boolean, number]
+
+const cokeTs: DrinkType = [
+  'brown', true, 60
+]
