fd2c5f2ccc98241682494084d03fca8171b75e95
bwapes <benwaples@gmail.com>
2020-12-22 13:50:11 -0800

    Car extends Vehicle

--- section8/functionalClasses.ts
+++ section8/functionalClasses.ts
@@ -3,4 +3,22 @@ class Vehicle {
   drive(): void {
     console.log('motor sounds')
   }
+  honk(): void {
+    console.log('HONK')
+  }
 }
+
+// copy and paste Vehicle methods onto car. Think of React
+class Car extends Vehicle {
+  drive(): void {
+    console.log('super fricken fast')
+  }
+}
+
+const vehicle = new Vehicle()
+// note the automagical stuff
+vehicle.drive()
+vehicle.honk()
+
+const car = new Car()
+car.drive()
