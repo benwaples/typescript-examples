68cd2da90c0aecb6720aab9b5ffb1c6c51acbc18
bwapes <benwaples@gmail.com>
2020-12-22 13:35:21 -0800

    interface is like minimum requirements

--- section7/interfaces.ts
+++ section7/interfaces.ts
@@ -6,11 +6,15 @@ interface CarType {
   model: string;
   year: number;
   runs: boolean;
+}
+
+
+interface Reportable {
   summary(): string; 
 }
 
 // inferred type
-const oldCamry: CarType = {
+const oldCamry = {
   make: 'toyota',
   model: 'camry',
   year: 1997,
@@ -20,8 +24,18 @@ const oldCamry: CarType = {
   }
 }
 
+const coke = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 50,
+  summary(): string {
+    return this.sugar
+  }
+}
+
 // functions that return nothing return void
-function printVehicle(car: CarType): void {
+// & in this case is saying that the car must be at least car type and reportable 
+function printVehicle(car: CarType & Reportable): void {
   console.log(`
     Name: ${car.model}
     Make: ${car.summary()}
@@ -30,4 +44,8 @@ function printVehicle(car: CarType): void {
   `)
 }
 
+// works because its a car and can be summarized
 printVehicle(oldCamry)
+
+// error even though it is reportable, it is not a car
+printVehicle(coke)
