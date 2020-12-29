ba90aacd27c308cdeec67c1e8f7b4874bc2fd8ed
bwapes <benwaples@gmail.com>
2020-12-29 11:15:19 -0800

    simple bubble sort alg O(n2)

--- section10/src/index.ts
+++ section10/src/index.ts
@@ -3,11 +3,21 @@ class Sorter {
     this.collection = collection;
   }
 
-  sort(): void {
+  bubbleSort(): void {
+    const { length } = this.collection;
 
+    for (let i = 0; i < length; i ++) {
+      for (let j = 0; j < length - i - 1; j ++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j]
+          this.collection[j] = this.collection[j+1]
+          this.collection[j+1] = temp
+        }
+      }
+    }
   }
 }
 
 const sorter =  new Sorter([ 12, 3, 5, 6, 23 ])
-sorter.sort()
+sorter.bubbleSort()
 console.log(sorter.collection);
