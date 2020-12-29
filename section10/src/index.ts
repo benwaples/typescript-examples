9c03b48e3ebbdf50cf664cdf7ad2ed721ac9934c
bwapes <benwaples@gmail.com>
2020-12-29 11:27:19 -0800

    bubbleSort notes

--- section10/src/index.ts
+++ section10/src/index.ts
@@ -6,9 +6,13 @@ class Sorter {
   bubbleSort(): void {
     const { length } = this.collection;
 
+    // keep track of number of iterations of the array
     for (let i = 0; i < length; i ++) {
+      // make sure that we exclude the previously largest number
       for (let j = 0; j < length - i - 1; j ++) {
+        // if left is larger than right
         if (this.collection[j] > this.collection[j + 1]) {
+          // swap the left and right
           const temp = this.collection[j]
           this.collection[j] = this.collection[j+1]
           this.collection[j+1] = temp
