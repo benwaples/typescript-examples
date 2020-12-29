e1df9debabf8a6d97130d5102c826e40b9b0ee95
bwapes <benwaples@gmail.com>
2020-12-29 15:08:19 -0800

    demo LinkedList extends Sorter2

--- section10/src/index.ts
+++ section10/src/index.ts
@@ -1,4 +1,5 @@
 import { CharacterCollection } from "./CharacterCollection";
+import { LinkedList } from "./LinkedList";
 import { NumberCollection } from "./NumberCollection";
 
 const numberCollection = new NumberCollection([4, 5, 2, -6, 8, 39, 44, -44])
@@ -8,3 +9,11 @@ console.log(numberCollection.data)
 const characterCollection = new CharacterCollection('fjiIpqBnNa')
 characterCollection.bubbleSortNumbers()
 console.log(characterCollection.data)
+
+const linkedList = new LinkedList()
+linkedList.add(23)
+linkedList.add(33)
+linkedList.add(-4)
+linkedList.add(300)
+linkedList.bubbleSortNumbers()
+linkedList.print()
