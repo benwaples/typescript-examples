a4d2a105a6ab264adbb233ca6718c71b3b652622
bwapes <benwaples@gmail.com>
2020-12-29 11:40:12 -0800

    type guard

--- section10/src/index.ts
+++ section10/src/index.ts
@@ -1,27 +1,37 @@
 class Sorter {
-  constructor(public collection: number[]) {
+  // Sorter can receive an array of numbers or a string
+  constructor(public collection: number[] | string) {
     this.collection = collection;
   }
 
-  bubbleSort(): void {
+  bubbleSortNumbers(): void {
     const { length } = this.collection;
 
-    // keep track of number of iterations of the array
-    for (let i = 0; i < length; i ++) {
-      // make sure that we exclude the previously largest number
-      for (let j = 0; j < length - i - 1; j ++) {
-        // if left is larger than right
-        if (this.collection[j] > this.collection[j + 1]) {
-          // swap the left and right
-          const temp = this.collection[j]
-          this.collection[j] = this.collection[j+1]
-          this.collection[j+1] = temp
+    // implicitly say that this loop will only ever run if the collection is an array
+    // this is called a type guard
+    if(this.collection instanceof Array) {
+      // keep track of number of iterations of the array
+      for (let i = 0; i < length; i ++) {
+        // make sure that we exclude the previously largest number
+        for (let j = 0; j < length - i - 1; j ++) {
+          // if left is larger than right
+          if (this.collection[j] > this.collection[j + 1]) {
+            // swap the left and right
+            const temp = this.collection[j]
+            this.collection[j] = this.collection[j+1]
+            this.collection[j+1] = temp
+          }
         }
       }
     }
   }
+
+  bubbleSortString(): void {
+
+    // logic will only ever work if the collection is a string
+  }
 }
 
 const sorter =  new Sorter([ 12, 3, 5, 6, 23 ])
-sorter.bubbleSort()
+sorter.bubbleSortNumbers()
 console.log(sorter.collection);
