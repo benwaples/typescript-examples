16591448c19a722a3adc2c8b6f526f8a103831c0
bwapes <benwaples@gmail.com>
2020-12-31 17:23:54 -0800

    extending an interface on a function

--- section12/generics.ts
+++ section12/generics.ts
@@ -55,7 +55,47 @@ const stringArray = ['a', 'b', 'c']
 const numberArray = [1, 2, 3, 4]
 
 // explicitly pass in the generics type 
+// helps to pass in the parameters type via the generic because it'll help you catch errors
 printAnything<string>(stringArray)
 
 // or you can have TS infer what type T will be by the parameter being passed in.
 printAnything(stringArray)
+
+// Generic Constraints
+class Car {
+  _id: string;
+
+  print() {
+    console.log('I am a car')
+  }
+}
+
+class House {
+  id: string;
+
+  print() {
+    console.log('I am a house')
+  }
+}
+
+// must add a constraint to the T in order to make sure that the parameter has a print method on it. 
+// add an interface to the parameter to make sure it satisfies the interface
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for(let i =0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+// a number array doesnt have a printable method on it so it errors.
+printHousesOrCars([1,2,4,3])
+
+// these objects will have a printable method on it so they are okay to pass.
+printHousesOrCars([new House(), new House(), new Car()])
+
+// however if you also pass a type through the generic, then the parameter must be of that type
+// the id attribute on House is different than on Car.
+printHousesOrCars<House>([new House(), new House(), new Car()])
