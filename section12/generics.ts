dd15a9bf92508c03e0781a8e2c579c29b6d11666
bwapes <benwaples@gmail.com>
2020-12-31 17:14:58 -0800

    function generics example

--- section12/generics.ts
+++ section12/generics.ts
@@ -1,3 +1,5 @@
+
+// generics class examples
 class ArrayOfNumbers {
   constructor(public collection: number[]) {}
 
@@ -25,3 +27,35 @@ class ArrayOfAnything<T> {
 
 // hover over stringArr and see how TS figures out how this is now an array of strings, not of type T
 const stringArr = new ArrayOfAnything<string>(['a', 'b'])
+
+// TS is smart enough that you don't need to pass in the generics in this instance, it can just figure out that the constructor will now be using a string type;
+const inference = new ArrayOfAnything(['a', 'b'])
+
+// generics example functions
+
+function printStrings(arr: string[]): void {
+  for(let i =0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+} 
+
+function printNumbers(arr: number[]): void {
+  for(let i =0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+} 
+
+function printAnything<T>(arr: T[]): void {
+  for(let i =0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+
+const stringArray = ['a', 'b', 'c']
+const numberArray = [1, 2, 3, 4]
+
+// explicitly pass in the generics type 
+printAnything<string>(stringArray)
+
+// or you can have TS infer what type T will be by the parameter being passed in.
+printAnything(stringArray)
