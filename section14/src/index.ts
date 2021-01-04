49b311e96bd916264480810fabd0ff3f2b24c8c3
bwapes <benwaples@gmail.com>
2021-01-03 17:33:43 -0800

    add router

--- section14/src/index.ts
+++ section14/src/index.ts
@@ -1,16 +1,19 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoute';
 
 const app = express()
 
 // Request and Response are classes that come along with the express app
 //  so we can use them to define the the parameters for an express endpoint
-app.get('/', (req: Request, res: Response) => {
+app.get('/test', (req: Request, res: Response) => {
   res.send(`
     <div>
       <h1>Hi There</h1>
     </div>
   `)
 })
+
+app.use(router)
 
 app.listen(3000, () => {
   console.log('Listening to port 3000')
