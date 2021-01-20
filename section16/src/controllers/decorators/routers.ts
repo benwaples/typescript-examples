a9e13561d4ff2eff3a20a51eb00be736420e4b62
bwapes <benwaples@gmail.com>
2021-01-20 15:54:25 -0800

    defineMetaData

--- section16/src/controllers/decorators/routers.ts
+++ section16/src/controllers/decorators/routers.ts
@@ -1,5 +1,8 @@
+import 'reflect-metadata'
+
 export function get(path: string) {
   return function(target: any, key: string, desc: PropertyDescriptor) {
-
+    // make use of the metadata package
+    Reflect.defineMetadata('path', path, target, key)
   }
 }
