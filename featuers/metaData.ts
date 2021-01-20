817b8f7504833484969049992f795c4f715e91c0
bwapes <benwaples@gmail.com>
2021-01-19 16:38:23 -0800

    meta data note

--- featuers/metaData.ts
+++ featuers/metaData.ts
@@ -4,12 +4,13 @@ const plane = {
   color: 'red',
 };
 
-Reflect.defineMetadata('note', 'hi there', plane)
-Reflect.defineMetadata('height', 10, plane)
+// example of adding metadata to an object using the Reflect package
+// Reflect.defineMetadata('note', 'hi there', plane)
+// Reflect.defineMetadata('height', 10, plane)
 
 
-const note = Reflect.getMetadata('note', plane)
-const height = Reflect.getMetadata('height', plane)
+// const note = Reflect.getMetadata('note', plane)
+// const height = Reflect.getMetadata('height', plane)
 
-console.log(note)
-console.log(height)
+// console.log(note)
+// console.log(height)
