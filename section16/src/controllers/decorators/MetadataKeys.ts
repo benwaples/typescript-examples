726df3ede91042ebd04c601b136450cb3f3824a2
bwapes <benwaples@gmail.com>
2021-01-22 17:37:23 -0800

    MetadataKeys notes

--- section16/src/controllers/decorators/MetadataKeys.ts
+++ section16/src/controllers/decorators/MetadataKeys.ts
@@ -1,3 +1,5 @@
+// abstracting this into enums is good because it means there are less chances for error
+// i wonder what kind of data can go into an enum
 export enum MetadataKeys {
   method = 'method',
   path = 'path'
